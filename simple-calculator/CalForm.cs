2600855d60a78b70851c19588e4633c17bf19a7e
dhk <2670065395@qq.com>
2024-08-22 17:26:33 +0800

    对我写的函数作出部分注释

--- simple-calculator/CalForm.cs
+++ simple-calculator/CalForm.cs
@@ -285,9 +285,18 @@ public partial class CalForm : Form
     }
 
 }
-//计算方法
+/// <summary>
+/// 计算方法类
+/// </summary>
 public static class AnyCalculate
 {
+    /// <summary>
+    /// 查找在str中从startIndex开始首个不属于anyOf的字符
+    /// </summary>
+    /// <param name="str">要查找的字符串全集</param>
+    /// <param name="anyOf">要查找字符目标集合</param>
+    /// <param name="startIndex">查找开始位置</param>
+    /// <returns>查找的结果，-1为没有找到</returns>
     public static int IndexNotOfAny(this string str, char[] anyOf, int startIndex)
     {
         for (int a = startIndex; a < str.Length; ++a)
@@ -306,24 +315,14 @@ public static class AnyCalculate
         }
         return -1;
     }
-    public static int LastIndexNotOfAny(this string str, char[] anyOf, int startIndex)
-    {
-        for (int a = startIndex; a > -1; --a)
-        {
-            char b = str[a];
-            bool c = true;
-            foreach (char d in anyOf)
-            {
-                if (b == d)
-                {
-                    c = false;
-                    break;
-                }
-            }
-            if (c) return a;
-        }
-        return -1;
-    }
+    /// <summary>
+    /// 逆序查找在str中从startIndex开始的count个字符中首个不属于anyOf的字符
+    /// </summary>
+    /// <param name="str">要查找的字符串全集</param>
+    /// <param name="anyOf">要查找字符目标集合</param>
+    /// <param name="startIndex">查找开始位置</param>
+    /// <param name="count">查找字符数量</param>
+    /// <returns>查找的结果，-1为没有找到</returns>
     public static int LastIndexNotOfAny(this string str, char[] anyOf, int startIndex, int count)
     {
         for (int a = startIndex; a > startIndex - count; --a) 
@@ -342,32 +341,47 @@ public static class AnyCalculate
         }
         return -1;
     }
-    //字符计数
+    /// <summary>
+    /// 计数str中从startIndex开始的count个字符中为value的字符数
+    /// </summary>
+    /// <param name="str">要查找的字符串全集</param>
+    /// <param name="Startindex">查找字符数量</param>
+    /// <param name="count">查找开始位置</param>
+    /// <param name="value">查找目标字符</param>
+    /// <returns>查找的结果数量</returns>
     public static int Count(this string str, int Startindex, int count, char value = ' ')
     {
         return str.AsSpan(Startindex, count).Count(value);
     }
-    //字符串转数字
-    private static double Svtoty(ref string a, int b = 0, int Base = 10)
+    /// <summary>
+    /// 将str从startIndex开始的连续字符序列转换为以Base为基数的double类型数
+    /// </summary>
+    /// <param name="str">要转换的字符串全集</param>
+    /// <param name="Base">基数</param>
+    /// <returns>转换为的浮点数</returns>
+    private static double Svtoty(ref string str, int Startindex = 0, int Base = 10)
     {
         double d = 0.0, e = Base;
-        int? f = null, g = a.Length;
-        for (; b < g && (a[b] >= '0' && a[b] <= '9' || a[b] == '.'); ++b)
+        int? f = null, g = str.Length;
+        for (; Startindex < g && (str[Startindex] >= '0' && str[Startindex] <= '9' || str[Startindex] == '.'); ++Startindex)
         {
             if (f != null) ++f;
-            if (a[b] == '.') f = 0;
+            if (str[Startindex] == '.') f = 0;
             else
             {
                 d *= e;
-                d += a[b] - '0';
+                d += str[Startindex] - '0';
             }
         }
         if (f != null)
             d /= Math.Pow(e, (double)f);
         return d;
     }
-
-    //数与符号分离
+    /// <summary>
+    /// 将str中数与符号分离
+    /// </summary>
+    /// <param name="str">要转换的字符串全集，转换为原字符串中的剩余符号集合</param>
+    /// <returns>字符串中的浮点数集合</returns>
     private static List<double> Zhuan(ref string str)
     {
         List<double> a = [];
@@ -382,7 +396,13 @@ public static class AnyCalculate
         }
         return a;
     }
-    //无括号部分计算
+    /// <summary>
+    /// 将范围中的算式计算为结果
+    /// </summary>
+    /// <param name="a">要计算的存有符号的字符串全集，计算将删除被计算的部分</param>
+    /// <param name="b">要计算的浮点数全集，计算将删除被计算的部分</param>
+    /// <param name="c">要计算的字符串开始字符</param>
+    /// <param name="d">要计算的字符串结束字符（左开右闭）</param>
     private static void Sshu(ref string a, ref List<double> b, int c, int d)
     {
         char[] de = ['+', '-'], df = ['*', '/'], dh = ['+', '-', '^'];
@@ -421,7 +441,11 @@ public static class AnyCalculate
             else b[f - 1] -= b[f];
         }
     }
-    //核心计算，对括号处理
+    /// <summary>
+    /// 将算式计算为结果
+    /// </summary>
+    /// <param name="expression">要计算的表达式</param>
+    /// <returns>计算结果</returns>
     public static double Calculate(string expression)
     {
         List<double> c = Zhuan(ref expression);

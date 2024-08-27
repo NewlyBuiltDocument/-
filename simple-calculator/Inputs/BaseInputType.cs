909f1f1bc47f59fc0eb09c01ecfc4ef4aaf026ce
choco <z.0301@outlook.com>
2024-08-27 15:08:32 +0800

    docs: add some comments

--- simple-calculator/Inputs/BaseInputType.cs
+++ simple-calculator/Inputs/BaseInputType.cs
@@ -3,7 +3,7 @@ namespace simple_calculator.Inputs;
 /// <summary>
 /// 输入处理基类
 /// </summary>
-/// <param name="calculator"></param>
+/// <param name="calculator">计算器类</param>
 public abstract class BaseInputType(Calculator calculator)
 {
     public readonly Calculator calculator = calculator;
